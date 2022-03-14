d3b773a950e7f12686dfa19fd0d2a8f4879e10d8
qzydustin <qzydustin@hotmail.com>
2022-03-14 16:17:18 -0700

    Initialize backend

---
+++ backend/src/test/java/hat/cap/CourseAssistantPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package hat.cap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAssistantPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
