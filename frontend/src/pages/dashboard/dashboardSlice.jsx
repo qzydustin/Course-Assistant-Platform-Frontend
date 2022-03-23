f230f6cf4f0836204dd70f372af8050f1c6a1f30
MjolnirT <Tansuc@live.com>
2022-03-22 21:41:42 -0700

    Add dashboardslice

---
+++ frontend/src/pages/dashboard/dashboardSlice.jsx
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// const pageContent = {
+//     'Front page': [
+//         { id: 'Content', value: true},
+//         { id: 'Content1', value: false},
+//     ],
+//     'Switch 1': [
+//         { id: 'Content', value: false},
+//         { id: 'Content1', value: true},
+//     ],
+// }
+
+export const contentsSlice = createSlice({
+    name: 'contentsController',
+    initialState: {
+        isContentShown: 0
+        // isContentShown: false,
+        // isContent1Shown: false,
+    },
+    reducers: {
+        toSwitch1: (state) => {
+            state.isContentShown = 1;
+            // state.isContentShown = pageContent["Switch 1"][0].value;
+            // state.isContent1Shown = pageContent["Switch 1"][0].value;
+        },
+    }
+})
+
+// Action creators are generated for each case reducer function
+export const { toSwitch1 } = contentsSlice.actions
+
+export const selectController = (state) => state.contentsController.isContentShown
+
+export default contentsSlice.reducer
