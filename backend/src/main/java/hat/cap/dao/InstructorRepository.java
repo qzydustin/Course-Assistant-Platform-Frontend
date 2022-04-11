ae577517b55079aa4c70ae81e0e14f8618905b83
qzydustin <qzydustin@hotmail.com>
2022-04-11 01:25:11 -0700

    Delete useless file

--- backend/src/main/java/hat/cap/dao/InstructorRepository.java
+++
@@ -1,8 +0,0 @@
-package hat.cap.dao;
-
-import hat.cap.model.Instructor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface InstructorRepository  extends CrudRepository<Instructor,Long> {
-    Instructor findByEmail(String email);
-}
